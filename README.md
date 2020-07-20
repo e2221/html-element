d7eb61898370f2ae4f4edd7ddc12f3e4c3418887
krall <luke.kral@seznam.cz>
2020-07-20 13:40:54 +0200

    protected $render - variable with saved renderer function renderStartTag(), renderEndTag(), renderPrintStartTag(), renderPrintEndTag()

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # html-element
 Base class for setting and rendering HTML elements
 
+Class BaseElement works with Nette\Utils\Html.<br>
+You can simply set attributes and render Html elements.<br><br>
+
+There is no documentation yet, but you can see examples
+in file /vendor/e2221/example/example.php
+<br><br>
+This package works without Nette Framework.
+
+Requirements
+-
+PHP 7.4
+
+Installation
+-
+``composer require e2221/html-element ``
+
---
+++ example/example.php
@@ -0,0 +1,69 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+foreach (glob(__DIR__ . "/../src/*.php") as $filename)
+{
+    include $filename;
+}
+
+//hidden element
+\e2221\HtmElement\BaseElement::getStatic('div', ['class'=>'bg-primary'])    //el name = html element name, parameters => array of attributes
+    ->addElement(\e2221\HtmElement\BaseElement::getStatic('p')
+        ->addElement(\e2221\HtmElement\HrefElement::getStatic() // will add html element to the html element
+            ->setTextContent('link')    // will set text content
+            ->setHref('https://google.com')  // will set href link
+            ->setConfirmation('aaa')         // will set javascript confirmation
+            ->setClass('             myLinkClass') // will set class of element
+            ->setHidden()                         // will hide this element
+        )
+        ->setDataAttributes(['ajax' => 'false'])   // will set data-ajax="false" to attribute
+        ->setTitle('my title')              // will set attribute title
+        ->setAttributes(['title' => 'my title', 'anyattribute' => 'anyvalue']) // will set also title or any html attribute
+    )
+    ->renderPrint();
+
+echo '<br>';
+
+
+$test = new \e2221\HtmElement\BaseElement('a');
+echo $test->setElName('a')
+    ->setAttributes(['href' => 'seznam.cz'])
+    ->setTextContent('text')
+    ->render();
+
+echo '<br>';
+
+// href element extends BaseElement, but it´s prepared with href html attributes
+$a = new \e2221\HtmElement\HrefElement();
+$a->setHref('https://www.google.com', ['q' => 'searchtext'])
+    ->setTextContent('link')
+    ->setTargetBlank()
+    ->renderPrint();
+
+//you can simply get html Nette element
+$a->getElement(); //returns \Nette\Utils\Html instance
+
+echo '<br>';
+
+//you can also print only start|end tag
+echo $a->renderStartTag() . 'my content of element' . $a->renderEndTag();
+
+echo '<br>';
+
+//render + echo
+$a->renderPrintStartTag();
+    echo 'my another content';
+$a->renderPrintEndTag();
+
+
+
+echo '<br>';
+
+
+//href with javascript confirmation
+\e2221\HtmElement\HrefElement::getStatic()
+    ->setTextContent('newLink')
+    ->setHref('https://www.google.com', ['q' => 'filsearch with text'])
+    ->setTargetBlank()
+    ->setConfirmation('Are you confirm?')
+    ->renderPrint();
